57fd5101ab356978ffa1665cf2aeac4deaf151bc
atomy <atomy@jackinpoint.net>
2024-01-22 04:25:19 +0100

    - truncate gamefile on rcon-success

--- util/gamelog.go
+++ util/gamelog.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"log"
 	"os"
 )
 
@@ -14,4 +15,29 @@ func WriteToGameLogFile(content string) {
 	}
 
 	WriteContentToFile(gameLogPath, content)
+}
+
+// TruncateGameLogFile Truncate game-log for a fresh start.
+func TruncateGameLogFile() {
+	gameLogPath := os.Getenv("GAME_LOGPATH")
+
+	// If env is empty, skip this.
+	if gameLogPath == "" {
+		return
+	}
+
+	// Open the file for writing
+	file, err := os.OpenFile(gameLogPath, os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(file *os.File) {
+		_ = file.Close()
+	}(file)
+
+	// Truncate the file to 100 bytes
+	err = file.Truncate(100)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
